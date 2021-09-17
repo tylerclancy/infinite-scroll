9cf944cb5cd2d864a02ca430692da49c6e65fc86
Tyler <tylerclancy030@gmail.com>
2021-09-16 20:38:03 -0500

    Decrease initial load time

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ let ready = false;
 let imagesLoaded = 0;
 let totalImages = 0;
 let photosArray = [];
+let initialLoad = true;
 
 // Unsplash API
-const count = 10;
+let count = 5;
 const apiKey = 'API_KEY_HERE';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
@@ -17,6 +18,9 @@ function imageLoaded() {
   if (imagesLoaded === totalImages) {
     ready = true;
     loader.hidden = true;
+    initialLoad = false;
+    count = 20;
+    apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
   }
 }
 
